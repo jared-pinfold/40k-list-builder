bdf8a3ff8e2a067c55ea2fc14969ba16f968a45e
Jared Pinfold <j.pinfold@gmail.com>
2023-06-18 16:03:53 +1200

    basic layout done, and data file created

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react"
+import { data, Unit } from "../data"
+
 function App() {
+  const [points, setPoints] = useState(0)
+  const [pool, setPool] = useState(data)
+  const [list, setList] = useState([] as Unit[])
+
   return (
     <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
+      <h1>{`Jared's Warhammer 40k List builder`}</h1>
+      <p>Click add to move a unit into the army list, and remove to move it back to the pool</p>
+      <div className="container">
+        <section className="column">
+          <h2>Pool</h2>
+          <ul>
+            {pool.map(unit => <li key={unit.id}>
+              <h3>{unit.name} - {unit.points}pts</h3>
+              <p>
+                Models: {unit.models}
+                Wargear: {unit.wargear}
+                </p>
+            </li>)}
+          </ul>
+        </section>
+        <section className="column">
+          <h2>Army - {points} points</h2>
+        </section>
+      </div>
+    
     </div>
   )
 }
---
+++ client/data.ts
@@ -0,0 +1,17 @@
+export interface Unit {
+  id: number
+  name: string
+  models: number
+  wargear: string
+  points: number
+}
+
+export const data: Unit[] = [
+  {
+    id: 1,
+  name: "Chaplain on Bike",
+  models: 1,
+  wargear: "Crozius Arcanum, Absolver Pistol, Twin Bolt Rifles",
+  points: 90
+  }
+]
