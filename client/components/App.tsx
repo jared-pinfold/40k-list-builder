36ba12f909070a802250f751557cc820e4fc7b99
Jared Pinfold <j.pinfold@gmail.com>
2023-06-19 14:52:46 +1200

    add and remove units functionality is done

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,33 +1,64 @@
-import { useState } from "react"
-import { data, Unit } from "../data"
+import { useEffect, useState } from 'react'
+import { data, Unit } from '../data'
 
 function App() {
   const [points, setPoints] = useState(0)
   const [pool, setPool] = useState(data)
   const [list, setList] = useState([] as Unit[])
 
+  useEffect(() => {
+    setPoints(list.reduce((a,c) => a + c.points, 0))
+  }, [pool, list])
+
+  function handleAdd(unit: Unit) {
+    setList([...list, unit])
+    setPool(pool.filter(currentUnit => currentUnit !== unit))
+  }
+
+  function handleRemove(unit: Unit) {
+    setPool([...pool, unit])
+    setList(list.filter(currentUnit => currentUnit !== unit))
+  }
+
   return (
     <div>
       <h1>{`Jared's Warhammer 40k List builder`}</h1>
-      <p>Click add to move a unit into the army list, and remove to move it back to the pool</p>
+      <p>
+        Click <b>add</b> to move a unit into the army list, and <b>remove</b> to
+        move it back to the pool
+      </p>
       <div className="container">
         <section className="column">
           <h2>Pool</h2>
-          <ul>
-            {pool.map(unit => <li key={unit.id}>
-              <h3>{unit.name} - {unit.points}pts</h3>
+
+          {pool.map((unit) => (
+            <article key={`pool${unit.id}`}>
+              <h3>
+                {unit.models} {unit.name} - {unit.points}pts{' '}
+                <button onClick={() =>handleAdd(unit)}>Add</button>
+              </h3>
               <p>
-                Models: {unit.models}
                 Wargear: {unit.wargear}
-                </p>
-            </li>)}
-          </ul>
+              </p>
+            </article>
+          ))}
         </section>
         <section className="column">
-          <h2>Army - {points} points</h2>
+          <h2>Army - {points} points <button>Export</button></h2>
+
+          {list.map((unit) => (
+            <article key={`list${unit.id}`}>
+              <h3>
+              {unit.models} {unit.name} - {unit.points}pts{' '}
+                <button onClick={() => handleRemove(unit)}>Remove</button>
+              </h3>
+              <p>
+                Wargear: {unit.wargear}
+              </p>
+            </article>
+          ))}
         </section>
       </div>
-    
     </div>
   )
 }
--- client/data.ts
+++ client/data.ts
@@ -9,9 +9,72 @@ export interface Unit {
 export const data: Unit[] = [
   {
     id: 1,
-  name: "Chaplain on Bike",
-  models: 1,
-  wargear: "Crozius Arcanum, Absolver Pistol, Twin Bolt Rifles",
-  points: 90
-  }
+    name: 'Chaplain on Bike',
+    models: 1,
+    wargear: 'Crozius Arcanum, Absolver Pistol, Twin Bolt Rifles',
+    points: 80,
+  },
+  {
+    id: 2,
+    name: 'Gravis Captain',
+    models: 1,
+    wargear: 'Power Sword, Boltstorm Gauntlet',
+    points: 95,
+  },
+  {
+    id: 3,
+    name: 'Inceptors',
+    models: 3,
+    wargear: 'Assault Bolters',
+    points: 115,
+  },
+  {
+    id: 4,
+    name: 'Primaris Chaplain',
+    models: 1,
+    wargear: 'Crozius Arcanum, Absolver Pistol',
+    points: 65,
+  },
+  {
+    id: 5,
+    name: 'Primaris Lt',
+    models: 1,
+    wargear: 'Stalker Bolter, Power Sword',
+    points: 75,
+  },
+  {
+    id: 6,
+    name: 'Primaris Lt',
+    models: 1,
+    wargear: 'Bolt Pistol, Power Sword',
+    points: 75,
+  },
+  {
+    id: 7,
+    name: 'Librarian',
+    models: 1,
+    wargear: 'Force Sword, Boltstorm Gauntlet',
+    points: 95,
+  },
+  {
+    id: 8,
+    name: 'Inceptors',
+    models: 3,
+    wargear: 'Assault Bolters',
+    points: 115,
+  },
+  {
+    id: 9,
+    name: 'Primaris Chaplain',
+    models: 1,
+    wargear: 'Crozius Arcanum, Absolver Pistol',
+    points: 65,
+  },
+  {
+    id: 10,
+    name: 'Primaris Lt',
+    models: 1,
+    wargear: 'Stalker Bolter, Power Sword',
+    points: 75,
+  },
 ]
