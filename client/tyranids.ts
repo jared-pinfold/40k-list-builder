8e692a997162664319759b9e94dd61048631314c
Jared Pinfold <j.pinfold@gmail.com>
2023-06-27 12:07:18 +1200

    Add Tyranid unit data

---
+++ client/tyranids.ts
@@ -0,0 +1,88 @@
+import { Unit } from "./models"
+
+export const tyranidsData: Unit[] = [
+  {
+    id: 1,
+    name: 'Screamer Killer',
+    models: 1,
+    wargear: 'Bio-plasmic Scream, Screamer-killer Talons',
+    points: 180,
+  },
+  {
+    id: 2,
+    name: 'Psychophage',
+    models: 1,
+    wargear: 'Psychoclastic Torrent, Talons & Tentacled Maw',
+    points: 125,
+  },
+  {
+    id: 3,
+    name: 'Barbgaunts',
+    models: 5,
+    wargear: 'Bio-cannon, Xenos Claws & Teeth',
+    points: 50,
+  },
+  {
+    id: 4,
+    name: 'Neurotyrant',
+    models: 1,
+    wargear: 'Psychic Scream, Neurotyran Claws & Lashes',
+    points: 105,
+  },
+  {
+    id: 5,
+    name: "Von Ryan's Leapers",
+    models: 3,
+    wargear: "Leaper's Talons",
+    points: 75,
+  },
+  {
+    id: 6,
+    name: 'Neurogaunts',
+    models: 11,
+    wargear: 'Xenos Claws & Teeth',
+    points: 45,
+  },
+  {
+    id: 7,
+    name: 'Winged Tyranid Prime',
+    models: 1,
+    wargear: 'Prime Talons',
+    points: 80,
+  },
+  {
+    id: 8,
+    name: 'Genestealers',
+    models: 10,
+    wargear: 'Genestealer Claws & Talons',
+    points: 180,
+  },
+  {
+    id: 9,
+    name: 'Ripper Swarms',
+    models: 3,
+    wargear: 'Spinemaws, Xenos Claws & Teeth',
+    points: 35,
+  },
+  {
+    id: 10,
+    name: 'Termagants',
+    models: 20,
+    wargear: 'Fleshborer, Xenos Claws & Teeth',
+    points: 120,
+  },
+  {
+    id: 11,
+    name: 'Genestealers',
+    models: 5,
+    wargear: 'Genestealer Claws & Talons',
+    points: 90,
+  },
+  {
+    id: 12,
+    name: 'Brood Lord',
+    models: 1,
+    wargear: 'Broodlord Claws & Teeth',
+    points: 100,
+  },
+]
